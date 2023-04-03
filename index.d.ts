0854da073c58727aaf45e2c7c96bdce285e69116
Takeru <takeru.moe@gmail.com>
2023-04-03 15:56:53 +0800

    chore: add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+import FormDevToolMixin from "./lib/FormDevToolMixin";
+import createApi from "./lib/createApi";
+import promptForChoice from "./lib/promptForChoice";
+
+export {
+    createApi,
+    promptForChoice,
+    FormDevToolMixin
+}
